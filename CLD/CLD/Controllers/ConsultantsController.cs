f60f1d490ef9760244bc16135f7b8b2e714b3f19
Joshua Otter <joshuaotter@gmail.com>
2019-10-16 14:03:51 +0200

    fout?

---
+++ CLD/CLD/Controllers/ConsultantsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CLD.Data;
+using CLD.Models;
+
+namespace CLD.Controllers
+{
+    public class ConsultantsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConsultantsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Consultants
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Consultant.ToListAsync());
+        }
+
+        // GET: Consultants/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultant = await _context.Consultant
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+
+            return View(consultant);
+        }
+
+        // GET: Consultants/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Consultants/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,ImageUrl,Biography")] Consultant consultant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(consultant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consultant);
+        }
+
+        // GET: Consultants/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultant = await _context.Consultant.FindAsync(id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+            return View(consultant);
+        }
+
+        // POST: Consultants/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,ImageUrl,Biography")] Consultant consultant)
+        {
+            if (id != consultant.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(consultant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConsultantExists(consultant.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consultant);
+        }
+
+        // GET: Consultants/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultant = await _context.Consultant
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+
+            return View(consultant);
+        }
+
+        // POST: Consultants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var consultant = await _context.Consultant.FindAsync(id);
+            _context.Consultant.Remove(consultant);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConsultantExists(int id)
+        {
+            return _context.Consultant.Any(e => e.Id == id);
+        }
+    }
+}
