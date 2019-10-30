9d455cd76e880a1caa37db0943266a79c686a001
Joshua Otter <joshuaotter@gmail.com>
2019-10-30 13:25:33 +0100

    userviewmodel update ding

--- CLD/CLD/Data/Migrations/20191029100023_eerste.Designer.cs
+++ CLD/CLD/Data/Migrations/20191029100023_eerste.Designer.cs
@@ -4,14 +4,16 @@ using CLD.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CLD.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191029100023_eerste")]
+    partial class eerste
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
